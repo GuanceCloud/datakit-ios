3ae581f92444980749307c4b37b19b2e85b66b86
hulilei <huuuu1016@gmail.com>
2019-12-09 15:32:36 +0800

    参数配置

--- zyft-sdk-ios/zyft-sdk-ios/Interceptor/ZYConfig.h
+++ zyft-sdk-ios/zyft-sdk-ios/Interceptor/ZYConfig.h
@@ -8,14 +8,11 @@
 
 #import <Foundation/Foundation.h>
 /* SDK版本 */
-#define ZG_SDK_VERSION @"1.0.0"
+#define ZY_SDK_VERSION @"1.0.0"
 
 /* 默认应用版本 */
-#define ZG_APP_VERSION [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
+#define ZY_APP_VERSION [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
 
-/*应用名称*/
-
-#define ZG_APP_NAME [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
 
 /* 渠道 */
 #define ZG_CHANNEL @"App Store"
@@ -26,33 +23,16 @@ NS_ASSUME_NONNULL_BEGIN
 // SDK版本
 @property (nonatomic, copy) NSString *sdkVersion;
 // 应用版本(默认:info.plist中CFBundleShortVersionString对应的值)
+@property (nonatomic, copy) NSString *sdkUUID;
 @property (nonatomic, copy) NSString *appVersion;
+@property (nonatomic, copy) NSString *akId;
+@property (nonatomic, copy) NSString *akSecret;
 //应用名称（默认：info.plist中的CFBundleDisplayName）
 @property (nonatomic ,copy)NSString *appName;
 // 渠道(默认:@"App Store")
 @property (nonatomic, copy) NSString *channel;
-// 两次会话时间间隔(默认:30秒)
-@property (nonatomic) NSUInteger sessionInterval;
 
-#pragma mark - 发送策略
-// 上报时间间隔(默认:10秒)
-@property  NSUInteger sendInterval;
-// 每天最大上报事件数，超出部分缓存到本地(默认:500个)
-@property (nonatomic) NSUInteger sendMaxSizePerDay;
-// 本地缓存事件数(默认:500个)
-@property (nonatomic) NSUInteger cacheMaxSize;
-
-#pragma mark - 日志
-// 是否开启会话追踪(默认:开启)
-@property (nonatomic) BOOL sessionEnable;
-
-@property (nonatomic) BOOL exceptionTrack;
-// 是否开启实时调试(默认:关闭)
-@property (nonatomic) BOOL debug;
-
-
-// 是否推送到生产环境，默认YES(推送时指定deviceToken上传到开发环境或生产环境)
-@property (nonatomic) BOOL apsProduction;
+@property (nonatomic, assign) BOOL enableRequestSigning;
 @end
 
 NS_ASSUME_NONNULL_END
