e208d169cd9f444d0606e548c6137e3fc2a8baef
hulilei <huuuu1016@gmail.com>
2019-12-23 10:11:11 +0800

    测试UI VC添加

---
+++ ft-sdk-iosTest/UITestVC.m
@@ -0,0 +1,32 @@
+//
+//  UITestVC.m
+//  ft-sdk-iosTest
+//
+//  Created by 胡蕾蕾 on 2019/12/20.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "UITestVC.h"
+
+@interface UITestVC ()
+
+@end
+
+@implementation UITestVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
