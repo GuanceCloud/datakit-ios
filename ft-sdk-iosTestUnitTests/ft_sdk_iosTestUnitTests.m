264317bdb90edf51913826d9749e13cf66276c37
hulilei <huuuu1016@gmail.com>
2019-12-23 17:23:34 +0800

    iosTest add

---
+++ ft-sdk-iosTestUnitTests/ft_sdk_iosTestUnitTests.m
@@ -0,0 +1,38 @@
+//
+//  ft_sdk_iosTestUnitTests.m
+//  ft-sdk-iosTestUnitTests
+//
+//  Created by 胡蕾蕾 on 2019/12/19.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <FTMobileAgent/>
+@interface ft_sdk_iosTestUnitTests : XCTestCase
+
+@end
+
+@implementation ft_sdk_iosTestUnitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+   
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
