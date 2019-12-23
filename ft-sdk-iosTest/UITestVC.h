bb3969ac43632d1ce9054992288fc5ccbcb54643
hulilei <huuuu1016@gmail.com>
2019-12-23 10:11:11 +0800

    测试UI VC添加

---
+++ ft-sdk-iosTest/UITestVC.h
@@ -0,0 +1,17 @@
+//
+//  UITestVC.h
+//  ft-sdk-iosTest
+//
+//  Created by 胡蕾蕾 on 2019/12/20.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITestVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
