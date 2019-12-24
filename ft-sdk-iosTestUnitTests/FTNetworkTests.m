3b21bf322893fd46db39f9c2daa7e2386ea2bc88
hulilei <huuuu1016@gmail.com>
2019-12-24 20:30:34 +0800

    test add

---
+++ ft-sdk-iosTestUnitTests/FTNetworkTests.m
@@ -0,0 +1,37 @@
+//
+//  FTNetworkTests.m
+//  ft-sdk-iosTestUnitTests
+//
+//  Created by 胡蕾蕾 on 2019/12/24.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FTNetworkTests : XCTestCase
+
+@end
+
+@implementation FTNetworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
