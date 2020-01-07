bc0bc358ac5f428ae0513cc1d734f49c2d681ed0
hulilei <huuuu1016@gmail.com>
2020-01-07 19:09:42 +0800

    可用内存、电池电量获取方法添加

--- ft-sdk-ios/FTMobileAgent/Interceptor/ZYBaseInfoHander.m
+++ ft-sdk-ios/FTMobileAgent/Interceptor/ZYBaseInfoHander.m
@@ -356,4 +356,25 @@
     }
     return @"";
 }
++(double)deviceLevel{
+    [UIDevice currentDevice].batteryMonitoringEnabled = YES;
+    double deviceLevel = [UIDevice currentDevice].batteryLevel;
+    return deviceLevel;
+}
+//当前设备可用内存
++ (double)availableMemory
+{
+    vm_statistics_data_t vmStats;
+    mach_msg_type_number_t infoCount = HOST_VM_INFO_COUNT;
+    kern_return_t kernReturn = host_statistics(mach_host_self(),
+                                               HOST_VM_INFO,
+                                               (host_info_t)&vmStats,
+                                               &infoCount);
+    
+    if (kernReturn != KERN_SUCCESS) {
+        return NSNotFound;
+    }
+    
+    return ((vm_page_size * vmStats.free_count) / 1024.0) / 1024.0;
+}
 @end
