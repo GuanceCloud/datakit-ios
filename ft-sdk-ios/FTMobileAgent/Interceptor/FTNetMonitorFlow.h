b419ecf3e9ea80b333daadd0c51b5d565a0aa25a
hulilei <huuuu1016@gmail.com>
2020-01-09 18:19:24 +0800

    实时监测网速

---
+++ ft-sdk-ios/FTMobileAgent/Interceptor/FTNetMonitorFlow.h
@@ -0,0 +1,24 @@
+//
+//  network.h
+//  testdemo
+//
+//  Created by 胡蕾蕾 on 2020/1/9.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTNetMonitorFlow : NSObject
+//开始检测
+@property (nonatomic, copy) void(^updateNetFlowBlock)(NSString *flow);
+
+- (void)startMonitor;
+
+//停止检测
+
+- (void)stopMonitor;
+@end
+
+NS_ASSUME_NONNULL_END
