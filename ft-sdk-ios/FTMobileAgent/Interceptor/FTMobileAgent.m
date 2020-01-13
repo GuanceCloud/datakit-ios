87214b04b8fae172485e96845baec4c4434d28b1
hulilei <huuuu1016@gmail.com>
2020-01-13 19:02:34 +0800

    用户绑定

--- ft-sdk-ios/FTMobileAgent/Interceptor/FTMobileAgent.m
+++ ft-sdk-ios/FTMobileAgent/Interceptor/FTMobileAgent.m
@@ -102,8 +102,7 @@ static void ZYReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
         }
         }
         self.upTool = [[ZYUploadTool alloc]initWithConfig:self.config];
-
-//        [self dealMonitorInfoType];
+        self.upTool.net =[@"-1" isEqualToString:self.net]?@"未知":[@"0" isEqualToString:self.net]?@"移动网络":@"WIFI";
        
     }
     return self;
@@ -163,6 +162,7 @@ static void ZYReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
         self.net = @"-1";//未知
     }
     ZYDebug(@"联网状态: %@", [@"-1" isEqualToString:self.net]?@"未知":[@"0" isEqualToString:self.net]?@"移动网络":@"WIFI");
+    self.upTool.net =[@"-1" isEqualToString:self.net]?@"未知":[@"0" isEqualToString:self.net]?@"移动网络":@"WIFI";
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification {
@@ -221,6 +221,10 @@ static void ZYReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
 }
 
 - (void)bindUserWithName:(NSString *)name Id:(NSString *)Id exts:(NSDictionary *)exts{
+    if (name.length == 0 || Id.length == 0) {
+        ZYDebug(@"绑定用户失败！！！ 用户名和用户Id 不能为空");
+        return;
+    }
     [[ZYTrackerEventDBTool sharedManger] insertUserDataWithName:name Id:Id exts:exts];
 }
 - (void)logout{
