30230af209c84cf38f53e1f617a90e507043c846
hulilei <huuuu1016@gmail.com>
2020-01-14 15:47:15 +0800

    网络强度 网络类型

---
+++ ft-sdk-ios/FTMobileAgent/Interceptor/FTNetworkInfo.m
@@ -0,0 +1,269 @@
+//
+//  FTNetworkInfo.m
+//  testdemo
+//
+//  Created by 胡蕾蕾 on 2020/1/14.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import "FTNetworkInfo.h"
+#import "AppDelegate.h"
+#define kDevice_Is_iPhoneX \
+({BOOL isPhoneX = NO;\
+if (@available(iOS 11.0, *)) {\
+isPhoneX = [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom > 0.0;\
+}\
+(isPhoneX);})
+@implementation FTNetworkInfo
++ (NSString *)getNetworkType
+{
+    UIApplication *app = [UIApplication sharedApplication];
+     id statusBar = nil;
+//    判断是否是iOS 13
+    NSString *network = @"";
+    if (@available(iOS 13.0, *)) {
+            // 需要在主线程执行的代码
+            UIStatusBarManager *statusBarManager = [UIApplication sharedApplication].keyWindow.windowScene.statusBarManager;
+       
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+        if ([statusBarManager respondsToSelector:@selector(createLocalStatusBar)]) {
+            UIView *localStatusBar = [statusBarManager performSelector:@selector(createLocalStatusBar)];
+            if ([localStatusBar respondsToSelector:@selector(statusBar)]) {
+                statusBar = [localStatusBar performSelector:@selector(statusBar)];
+            }
+        }
+#pragma clang diagnostic pop
+        
+        if (statusBar) {
+//            UIStatusBarDataCellularEntry
+            id currentData = [[statusBar valueForKeyPath:@"_statusBar"] valueForKeyPath:@"currentData"];
+            id _wifiEntry = [currentData valueForKeyPath:@"wifiEntry"];
+            id _cellularEntry = [currentData valueForKeyPath:@"cellularEntry"];
+            if (_wifiEntry && [[_wifiEntry valueForKeyPath:@"isEnabled"] boolValue]) {
+//                If wifiEntry is enabled, is WiFi.
+                network = @"WIFI";
+            } else if (_cellularEntry && [[_cellularEntry valueForKeyPath:@"isEnabled"] boolValue]) {
+                NSNumber *type = [_cellularEntry valueForKeyPath:@"type"];
+                if (type) {
+                    switch (type.integerValue) {
+                        case 0:
+//                            无sim卡
+                            network = @"NONE";
+                            break;
+                        case 1:
+                            network = @"1G";
+                            break;
+                        case 4:
+                            network = @"3G";
+                            break;
+                        case 5:
+                            network = @"4G";
+                            break;
+                        default:
+//                            默认WWAN类型
+                            network = @"WWAN";
+                            break;
+                            }
+                        }
+                    }
+                }
+    }else {
+        statusBar = [app valueForKeyPath:@"statusBar"];
+        
+        if (kDevice_Is_iPhoneX) {
+//            刘海屏
+                id statusBarView = [statusBar valueForKeyPath:@"statusBar"];
+                UIView *foregroundView = [statusBarView valueForKeyPath:@"foregroundView"];
+                NSArray *subviews = [[foregroundView subviews][2] subviews];
+                
+                if (subviews.count == 0) {
+//                    iOS 12
+                    id currentData = [statusBarView valueForKeyPath:@"currentData"];
+                    id wifiEntry = [currentData valueForKey:@"wifiEntry"];
+                    if ([[wifiEntry valueForKey:@"_enabled"] boolValue]) {
+                        network = @"WIFI";
+                    }else {
+//                    卡1:
+                        id cellularEntry = [currentData valueForKey:@"cellularEntry"];
+//                    卡2:
+                        id secondaryCellularEntry = [currentData valueForKey:@"secondaryCellularEntry"];
+
+                        if (([[cellularEntry valueForKey:@"_enabled"] boolValue]|[[secondaryCellularEntry valueForKey:@"_enabled"] boolValue]) == NO) {
+//                            无卡情况
+                            network = @"NONE";
+                        }else {
+//                            判断卡1还是卡2
+                            BOOL isCardOne = [[cellularEntry valueForKey:@"_enabled"] boolValue];
+                            int networkType = isCardOne ? [[cellularEntry valueForKey:@"type"] intValue] : [[secondaryCellularEntry valueForKey:@"type"] intValue];
+                            switch (networkType) {
+                                    case 0://无服务
+                                    network = [NSString stringWithFormat:@"%@-%@", isCardOne ? @"Card 1" : @"Card 2", @"NONE"];
+                                    break;
+                                    case 3:
+                                    network = [NSString stringWithFormat:@"%@-%@", isCardOne ? @"Card 1" : @"Card 2", @"2G/E"];
+                                    break;
+                                    case 4:
+                                    network = [NSString stringWithFormat:@"%@-%@", isCardOne ? @"Card 1" : @"Card 2", @"3G"];
+                                    break;
+                                    case 5:
+                                    network = [NSString stringWithFormat:@"%@-%@", isCardOne ? @"Card 1" : @"Card 2", @"4G"];
+                                    break;
+                                default:
+                                    break;
+                            }
+                            
+                        }
+                    }
+                
+                }else {
+                    
+                    for (id subview in subviews) {
+                        if ([subview isKindOfClass:NSClassFromString(@"_UIStatusBarWifiSignalView")]) {
+                            network = @"WIFI";
+                        }else if ([subview isKindOfClass:NSClassFromString(@"_UIStatusBarStringView")]) {
+                            network = [subview valueForKeyPath:@"originalText"];
+                        }
+                    }
+                }
+                
+            }else {
+//                非刘海屏
+                UIView *foregroundView = [statusBar valueForKeyPath:@"foregroundView"];
+                NSArray *subviews = [foregroundView subviews];
+                
+                for (id subview in subviews) {
+                    if ([subview isKindOfClass:NSClassFromString(@"UIStatusBarDataNetworkItemView")]) {
+                        int networkType = [[subview valueForKeyPath:@"dataNetworkType"] intValue];
+                        switch (networkType) {
+                            case 0:
+                                network = @"NONE";
+                                break;
+                            case 1:
+                                network = @"2G";
+                                break;
+                            case 2:
+                                network = @"3G";
+                                break;
+                            case 3:
+                                network = @"4G";
+                                break;
+                            case 5:
+                                network = @"WIFI";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+    }
+
+    if ([network isEqualToString:@""]) {
+        network = @"NO DISPLAY";
+    }
+    
+    return network;
+}
++ (int)getNetSignalStrength{
+    
+    int signalStrength = 0;
+//        判断是否为iOS 13
+        if (@available(iOS 13.0, *)) {
+            UIStatusBarManager *statusBarManager = [UIApplication sharedApplication].keyWindow.windowScene.statusBarManager;
+             
+            id statusBar = nil;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+            if ([statusBarManager respondsToSelector:@selector(createLocalStatusBar)]) {
+                UIView *localStatusBar = [statusBarManager performSelector:@selector(createLocalStatusBar)];
+                if ([localStatusBar respondsToSelector:@selector(statusBar)]) {
+                    statusBar = [localStatusBar performSelector:@selector(statusBar)];
+                }
+            }
+#pragma clang diagnostic pop
+            if (statusBar) {
+                id currentData = [[statusBar valueForKeyPath:@"_statusBar"] valueForKeyPath:@"currentData"];
+                if([[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]){
+                    id wifiEntry = [currentData valueForKeyPath:@"wifiEntry"];
+                    if ([wifiEntry isKindOfClass:NSClassFromString(@"_UIStatusBarDataIntegerEntry")]) {
+                        signalStrength = [[wifiEntry valueForKey:@"displayValue"] intValue];
+                    }
+                }
+                if (![[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]) {
+                     id cellularEntry  = [currentData valueForKeyPath:@"cellularEntry"];
+                  if ([cellularEntry isKindOfClass:NSClassFromString(@"_UIStatusBarDataCellularEntry")]) {
+                         //                    层级：_UIStatusBarDataNetworkEntry、_UIStatusBarDataIntegerEntry、_UIStatusBarDataEntry
+                                             
+                             signalStrength = [[cellularEntry valueForKey:@"displayValue"] intValue];
+                         }
+                }
+            }
+        }else {
+            UIApplication *app = [UIApplication sharedApplication];
+            id statusBar = [app valueForKey:@"statusBar"];
+            if (kDevice_Is_iPhoneX) {
+//                刘海屏
+                id statusBarView = [statusBar valueForKeyPath:@"statusBar"];
+                UIView *foregroundView = [statusBarView valueForKeyPath:@"foregroundView"];
+                NSArray *subviews = [[foregroundView subviews][2] subviews];
+                       
+                if (subviews.count == 0) {
+//                    iOS 12
+                    id currentData = [statusBarView valueForKeyPath:@"currentData"];
+                   if([[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]){
+                        id wifiEntry = [currentData valueForKeyPath:@"wifiEntry"];
+                        if ([wifiEntry isKindOfClass:NSClassFromString(@"_UIStatusBarDataIntegerEntry")]) {
+                            signalStrength = [[wifiEntry valueForKey:@"displayValue"] intValue];
+                        }
+                    }
+                    if (![[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]) {
+                         id cellularEntry  = [currentData valueForKeyPath:@"cellularEntry"];
+                      if ([cellularEntry isKindOfClass:NSClassFromString(@"_UIStatusBarDataCellularEntry")]) {
+                             //                    层级：_UIStatusBarDataNetworkEntry、_UIStatusBarDataIntegerEntry、_UIStatusBarDataEntry
+                                                 
+                                 signalStrength = [[cellularEntry valueForKey:@"displayValue"] intValue];
+                             }
+                    }
+
+                }else {
+                    for (id subview in subviews) {
+                        if([subview isKindOfClass:[NSClassFromString(@"UIStatusBarDataNetworkItemView") class]] && [[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]) {
+                            signalStrength = [[subview valueForKey:@"_wifiStrengthBars"] intValue];
+                            break;
+                        }
+                        if ([subview isKindOfClass:[NSClassFromString(@"UIStatusBarSignalStrengthItemView") class]] && ![[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]) {
+                            signalStrength = [[subview valueForKey:@"_signalStrengthRaw"] intValue];
+                            break;
+                        }
+                    }
+                }
+            }else {
+//                非刘海屏
+                UIView *foregroundView = [statusBar valueForKey:@"foregroundView"];
+                     
+                NSArray *subviews = [foregroundView subviews];
+                NSString *dataNetworkItemView = nil;
+                       
+                for (id subview in subviews) {
+                    if([subview isKindOfClass:[NSClassFromString(@"UIStatusBarDataNetworkItemView") class]] && [[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]) {
+                        dataNetworkItemView = subview;
+                        signalStrength = [[dataNetworkItemView valueForKey:@"_wifiStrengthBars"] intValue];
+                        break;
+                    }
+                    if ([subview isKindOfClass:[NSClassFromString(@"UIStatusBarSignalStrengthItemView") class]] && ![[self getNetworkType] isEqualToString:@"WIFI"] && ![[self getNetworkType] isEqualToString:@"NONE"]) {
+                        dataNetworkItemView = subview;
+                        signalStrength = [[dataNetworkItemView valueForKey:@"_signalStrengthRaw"] intValue];
+                        break;
+                    }
+                }
+               
+                        
+                return signalStrength;
+            }
+        }
+    
+    return signalStrength;
+}
+
+@end
