6684c2c699db64d95d2e6802f5e4d4caf50c6e9c
hulilei <huuuu1016@gmail.com>
2020-01-14 15:47:15 +0800

    网络强度 网络类型

---
+++ ft-sdk-ios/FTMobileAgent/Interceptor/FTNetworkInfo.h
@@ -0,0 +1,18 @@
+//
+//  FTNetworkInfo.h
+//  testdemo
+//
+//  Created by 胡蕾蕾 on 2020/1/14.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTNetworkInfo : NSObject
++ (NSString *)getNetworkType;
++ (int)getNetSignalStrength;
+@end
+
+NS_ASSUME_NONNULL_END
