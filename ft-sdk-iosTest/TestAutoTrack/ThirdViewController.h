8bf8ff4477c69d0ac22e9c7479b82322e150a8f6
hulilei <huuuu1016@gmail.com>
2020-02-26 19:17:26 +0800

    add

--- ft-sdk-iosTest/TestAutoTrack/ThirdViewController.h
+++ ft-sdk-iosTest/TestAutoTrack/ThirdViewController.h
@@ -1,17 +1,16 @@
 //
-//  UIViewController+FlowChart.h
-//  FTAutoTrack
+//  ThirdViewController.h
+//  ft-sdk-iosTest
 //
-//  Created by 胡蕾蕾 on 2020/2/25.
+//  Created by 胡蕾蕾 on 2020/2/26.
 //  Copyright © 2020 hll. All rights reserved.
 //
-
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface UIViewController (FlowChart)
+@interface ThirdViewController : UIViewController
 
 @end
 
