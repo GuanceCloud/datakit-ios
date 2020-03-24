06797d47c992d4473117b9c6efb7ee4aa762ef9c
hulilei <huuuu1016@gmail.com>
2020-03-24 15:57:17 +0800

    错误码处理

--- ft-sdk-ios/FTMobileAgent/FTMobileConfig.h
+++ ft-sdk-ios/FTMobileAgent/FTMobileConfig.h
@@ -12,10 +12,10 @@
 /* 默认应用版本 */
 #define FT_APP_VERSION [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
 typedef enum FTError : NSInteger {
-  InvalidParamsException = 102,//参数问题
-  NetWorkException = 101,//网络问题
-  FileIOException = 103, //文件 IO 问题
-  UnkownException = 104,//未知问题
+  NetWorkException = 101,        //网络问题
+  InvalidParamsException = 102,  //参数问题
+  FileIOException = 103,         //文件 IO 问题
+  UnkownException = 104,         //未知问题
 } FTError;
 
 /**
