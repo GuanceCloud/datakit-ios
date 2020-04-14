240f07b649ce708146729e74c7342154661702ff
hulilei <huuuu1016@gmail.com>
2020-04-14 15:28:23 +0800

    monitorManager add

---
+++ ft-sdk-ios/FTMobileAgent/FTMonitorManager.h
@@ -0,0 +1,17 @@
+//
+//  FTMonitorManager.h
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2020/4/14.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTMonitorManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ft-sdk-ios/FTMobileAgent/FTMonitorManager.m
@@ -0,0 +1,13 @@
+//
+//  FTMonitorManager.m
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2020/4/14.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import "FTMonitorManager.h"
+
+@implementation FTMonitorManager
+
+@end
