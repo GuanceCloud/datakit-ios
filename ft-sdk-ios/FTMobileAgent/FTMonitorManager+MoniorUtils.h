f3b5fbd9170f9c25eba4679908794557f8c864c2
hulilei <huuuu1016@gmail.com>
2020-04-23 23:31:35 +0800

    添加分类管理监控项获取方法

---
+++ ft-sdk-ios/FTMobileAgent/FTMonitorManager+MoniorUtils.h
@@ -0,0 +1,24 @@
+//
+//  FTMonitorManager+MoniorUtils.h
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2020/4/22.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import "FTMonitorManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTMonitorManager (MoniorUtils)
++ (NSString *)getLaunchSystemTime;
+
++ (NSString *)userDeviceName;
++ (NSDictionary *)getDNSInfo;
++ (NSDictionary *)getWifiAndIPAddress;
++ (NSString *)getCurrentWifiSSID;
++ (NSString *)getIPAddress;
++ (CGFloat)screenBrightness;
+@end
+
+NS_ASSUME_NONNULL_END
