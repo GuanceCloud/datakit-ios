712024561c55e07eb69437c4de3a84b59dc0130f
hulilei <huuuu1016@gmail.com>
2020-04-26 19:13:44 +0800

    监控项管理方法修改

--- ft-sdk-ios/FTMobileAgent/FTMonitorManager.m
+++ ft-sdk-ios/FTMobileAgent/FTMonitorManager.m
@@ -23,15 +23,15 @@
 #import "FTMonitorManager+MoniorUtils.h"
 #import "ZYLog.h"
 #import "FTUploadTool.h"
+#import "FTCMMotionManager.h"
 #define WeakSelf __weak typeof(self) weakSelf = self;
 
-@interface FTMonitorManager ()<CBCentralManagerDelegate,CBPeripheralDelegate,FTHTTPProtocolDelegate,CBPeripheralManagerDelegate>
-@property (nonatomic, strong) CBPeripheralManager *peripheralManager;
+@interface FTMonitorManager ()<CBCentralManagerDelegate,CBPeripheralDelegate,FTHTTPProtocolDelegate>
+@property (nonatomic, strong) CBCentralManager *centralManager;
 @property (nonatomic, strong) NSMutableArray *devicesListArray;
 @property (nonatomic, strong) FTMobileConfig *config;
 @property (nonatomic, strong) NSDictionary *monitorTagDict;
 @property (nonatomic, strong) FTNetMonitorFlow *netFlow;
-@property (nonatomic, strong) CMMotionManager *motionManager;
 @property (nonatomic, assign) CMAcceleration acceleration;
 @property (nonatomic, assign) NSInteger errorNet;
 @property (nonatomic, assign) NSInteger successNet;
@@ -61,7 +61,6 @@
         self.devicesListArray = [NSMutableArray new];
         self.config = config;
         self.metrics = [FTTaskMetrics new];
-        [self startMonitor];
         [self bluteeh];
     }
     return self;
@@ -78,7 +77,11 @@
             [[FTLocationManager sharedInstance] startUpdatingLocation];
         }
     }
-    [self startUpdateAccelerometerResult];
+    if (_config.monitorInfoType & FTMonitorInfoTypeSensor || _config.monitorInfoType & FTMonitorInfoTypeAll) {
+        [[FTCMMotionManager shared] startMotionUpdate];
+    }else{
+        [[FTCMMotionManager shared] stopMotionUpdates];
+    }
 }
 -(FTNetMonitorFlow *)netFlow{
     if (!_netFlow) {
@@ -109,7 +112,7 @@
     [[FTMobileAgent sharedInstance] performSelector:@selector(trackUpload:callBack:) withObject:@[model] withObject:UploadResultBlock];
 }
 #pragma mark ========== 网络请求相关时间 ==========
-- (void)startMonitor{
+- (void)startNetMonitor{
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dealTaskMetrics:) name:FTTaskMetricsNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dealTaskStates:) name:FTTaskCompleteStatesNotification object:nil];
 }
@@ -224,7 +227,10 @@
     }
     if (self.config.monitorInfoType & FTMonitorInfoTypeSensor || self.config.monitorInfoType & FTMonitorInfoTypeAll) {
         [field setValue:[NSNumber numberWithFloat:[FTMonitorManager screenBrightness]] forKey:@"screen_brightness"];
-    }
+        [field setValue:[NSNumber numberWithBool:[FTMonitorManager getProximityState]] forKey:@"proximity"];
+        [field setValue:[NSNumber numberWithFloat:[FTMonitorManager getTorchLevel]] forKey:@"torch"];
+        [field addEntriesFromDictionary:[[FTCMMotionManager shared] getMotionDatas]];
+        }
     return @{@"field":field,@"tag":tag};
 }
 #pragma mark --------- 实时网速 ----------
@@ -244,28 +250,8 @@
 }
 #pragma mark ========== 蓝牙 ==========
 - (void)bluteeh{
-    self.peripheralManager = [[CBPeripheralManager alloc]initWithDelegate:self queue:nil];
-
-//    NSDictionary *options = @{CBCentralManagerOptionShowPowerAlertKey:@NO};
-//    self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:options];
-}
-//peripheralManager状态改变
-- (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral{
-    switch (peripheral.state) {
-            //在这里判断蓝牙设别的状态  当开启了则可调用  setUp方法(自定义)
-        case CBPeripheralManagerStatePoweredOn:
-            NSLog(@"powered on");
-//            [info setText:[NSString stringWithFormat:@"设备名%@已经打开，可以使用center进行连接",LocalNameKey]];
-            [self setUp];
-            break;
-        case CBPeripheralManagerStatePoweredOff:
-            NSLog(@"powered off");
-//            [info setText:@"powered off"];
-            break;
-            
-        default:
-            break;
-    }
+    NSDictionary *options = @{CBCentralManagerOptionShowPowerAlertKey:@NO};
+    self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:options];
 }
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
     NSString *strMessage = nil;
@@ -273,7 +259,7 @@
         case CBManagerStatePoweredOn: {
             ZYDebug(@"蓝牙开启且可用");
             //周边外设扫描
-            [self setUp];
+            [self.centralManager scanForPeripheralsWithServices:nil options:nil];
             return;
         }
             break;
@@ -316,176 +302,8 @@
     // advertisementData 设备广告标识
     // 一般把新扫描到的设备添加到一个数组中，并更新列表
 }
-#pragma mark ========== CMMotionManager ==========
--(CMMotionManager*)motionManager {
-    if (_motionManager == nil) {
-        _motionManager= [[CMMotionManager alloc]init];
-    }
-    return _motionManager;
-}
-//陀螺仪暂停
-- (void)stopUpdate{
-    if([self.motionManager isAccelerometerActive] == YES){
-        [self.motionManager stopAccelerometerUpdates];
-    }
-}
-- (void)startUpdateAccelerometerResult{//陀螺仪开始
-    if([self.motionManager isAccelerometerAvailable] == YES){
-        [self.motionManager setAccelerometerUpdateInterval:(double)self.config.flushInterval / 1000.0];
-        [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error){
-            if (!error) {
-                self.acceleration = accelerometerData.acceleration;
-            }
-        }];
-    }
-}
 -(void)dealloc{
     [self stopFlushTimer];
     self.netFlow = nil;
 }
-static NSString *const ServiceUUID1 =  @"FFF0";
-static NSString *const notiyCharacteristicUUID =  @"FFF1";
-static NSString *const readwriteCharacteristicUUID =  @"FFF2";
-static NSString *const ServiceUUID2 =  @"FFE0";
-static NSString *const readCharacteristicUUID =  @"FFE1";
-static NSString *const LocalNameKey =  @"myPeripheral";
-
-//配置bluetooch的
--(void)setUp{
-    //characteristics字段描述
-    CBUUID *CBUUIDCharacteristicUserDescriptionStringUUID = [CBUUID UUIDWithString:CBUUIDCharacteristicUserDescriptionString];
-    
-    /*
-     可以通知的Characteristic
-     properties：CBCharacteristicPropertyNotify
-     permissions CBAttributePermissionsReadable
-     */
-    CBMutableCharacteristic *notiyCharacteristic = [[CBMutableCharacteristic alloc]initWithType:[CBUUID UUIDWithString:notiyCharacteristicUUID] properties:CBCharacteristicPropertyNotify value:nil permissions:CBAttributePermissionsReadable];
-
-    /*
-     可读写的characteristics
-     properties：CBCharacteristicPropertyWrite | CBCharacteristicPropertyRead
-     permissions CBAttributePermissionsReadable | CBAttributePermissionsWriteable
-     */
-    CBMutableCharacteristic *readwriteCharacteristic = [[CBMutableCharacteristic alloc]initWithType:[CBUUID UUIDWithString:readwriteCharacteristicUUID] properties:CBCharacteristicPropertyWrite | CBCharacteristicPropertyRead value:nil permissions:CBAttributePermissionsReadable | CBAttributePermissionsWriteable];
-    //设置description
-    CBMutableDescriptor *readwriteCharacteristicDescription1 = [[CBMutableDescriptor alloc]initWithType: CBUUIDCharacteristicUserDescriptionStringUUID value:@"name"];
-    [readwriteCharacteristic setDescriptors:@[readwriteCharacteristicDescription1]];
-    
-
-    /*
-     只读的Characteristic
-     properties：CBCharacteristicPropertyRead
-     permissions CBAttributePermissionsReadable
-     */
-    CBMutableCharacteristic *readCharacteristic = [[CBMutableCharacteristic alloc]initWithType:[CBUUID UUIDWithString:readCharacteristicUUID] properties:CBCharacteristicPropertyRead value:nil permissions:CBAttributePermissionsReadable];
-
-    
-    //service1初始化并加入两个characteristics
-    CBMutableService *service1 = [[CBMutableService alloc]initWithType:[CBUUID UUIDWithString:ServiceUUID1] primary:YES];
-    NSLog(@"%@",service1.UUID);
-    
-    [service1 setCharacteristics:@[notiyCharacteristic,readwriteCharacteristic]];
-    
-    //service2初始化并加入一个characteristics
-    CBMutableService *service2 = [[CBMutableService alloc]initWithType:[CBUUID UUIDWithString:ServiceUUID2] primary:YES];
-    [service2 setCharacteristics:@[readCharacteristic]];
-    
-    //添加后就会调用代理的- (void)peripheralManager:(CBPeripheralManager *)peripheral didAddService:(CBService *)service error:(NSError *)error
-    [self.peripheralManager addService:service1];
-    [self.peripheralManager addService:service2];
-}
-//perihpheral添加了service
-- (void)peripheralManager:(CBPeripheralManager *)peripheral didAddService:(CBService *)service error:(NSError *)error{
-    if (error == nil) {
-        serviceNum++;
-    }
-
-    //因为我们添加了2个服务，所以想两次都添加完成后才去发送广播
-    if (serviceNum==2) {
-        //添加服务后可以在此向外界发出通告 调用完这个方法后会调用代理的
-        //(void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral error:(NSError *)error
-        [self.peripheralManager startAdvertising:@{
-                                              CBAdvertisementDataServiceUUIDsKey : @[[CBUUID UUIDWithString:ServiceUUID1],[CBUUID UUIDWithString:ServiceUUID2]],
-                                              CBAdvertisementDataLocalNameKey : LocalNameKey
-                                             }
-         ];
-        
-    }
-    
-}
-//peripheral开始发送advertising
-- (void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral error:(NSError *)error{
-    NSLog(@"in peripheralManagerDidStartAdvertisiong");
-}
-
-//订阅characteristics
--(void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic{
-    NSLog(@"订阅了 %@的数据",characteristic.UUID);
-    //每秒执行一次给主设备发送一个当前时间的秒数
-    timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(sendData:) userInfo:characteristic  repeats:YES];
-}
-
-//取消订阅characteristics
--(void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didUnsubscribeFromCharacteristic:(CBCharacteristic *)characteristic{
-    NSLog(@"取消订阅 %@的数据",characteristic.UUID);
-    //取消回应
-    [timer invalidate];
-}
-
-//发送数据，发送当前时间的秒数
--(BOOL)sendData:(NSTimer *)t {
-    CBMutableCharacteristic *characteristic = t.userInfo;
-    NSDateFormatter *dft = [[NSDateFormatter alloc]init];
-    [dft setDateFormat:@"ss"];
-    NSLog(@"%@",[dft stringFromDate:[NSDate date]]);
-    
-    //执行回应Central通知数据
-    return  [self.peripheralManager updateValue:[[dft stringFromDate:[NSDate date]] dataUsingEncoding:NSUTF8StringEncoding] forCharacteristic:(CBMutableCharacteristic *)characteristic onSubscribedCentrals:nil];
-    
-}
-
-
-//读characteristics请求
-- (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveReadRequest:(CBATTRequest *)request{
-    NSLog(@"didReceiveReadRequest");
-    //判断是否有读数据的权限
-    if (request.characteristic.properties & CBCharacteristicPropertyRead) {
-        NSData *data = request.characteristic.value;
-        [request setValue:data];
-        //对请求作出成功响应
-        [self.peripheralManager respondToRequest:request withResult:CBATTErrorSuccess];
-    }else{
-        [self.peripheralManager respondToRequest:request withResult:CBATTErrorWriteNotPermitted];
-    }
-}
-
-
-//写characteristics请求
-- (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveWriteRequests:(NSArray *)requests{
-    NSLog(@"didReceiveWriteRequests");
-    CBATTRequest *request = requests[0];
-    
-    //判断是否有写数据的权限
-    if (request.characteristic.properties & CBCharacteristicPropertyWrite) {
-        //需要转换成CBMutableCharacteristic对象才能进行写值
-        CBMutableCharacteristic *c =(CBMutableCharacteristic *)request.characteristic;
-        c.value = request.value;
-        [self.peripheralManager respondToRequest:request withResult:CBATTErrorSuccess];
-    }else{
-        [self.peripheralManager respondToRequest:request withResult:CBATTErrorWriteNotPermitted];
-    }
-    
-    
-}
-
-//
-- (void)peripheralManagerIsReadyToUpdateSubscribers:(CBPeripheralManager *)peripheral{
-    NSLog(@"peripheralManagerIsReadyToUpdateSubscribers");
-    
-}
-
-
-
-
 @end
