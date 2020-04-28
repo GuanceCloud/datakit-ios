418c9f229f41fb659be3dfa542198a8b7d6f1d2c
hulilei <huuuu1016@gmail.com>
2020-04-28 18:24:22 +0800

    监控项获取方法获取修改

---
+++ ft-sdk-ios/FTMobileAgent/FTMoniorUtils.h
@@ -0,0 +1,17 @@
+//
+//  FTMoniorUtils.h
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2020/4/28.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTMoniorUtils : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ft-sdk-ios/FTMobileAgent/FTMoniorUtils.m
@@ -0,0 +1,13 @@
+//
+//  FTMoniorUtils.m
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2020/4/28.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import "FTMoniorUtils.h"
+
+@implementation FTMoniorUtils
+
+@end
