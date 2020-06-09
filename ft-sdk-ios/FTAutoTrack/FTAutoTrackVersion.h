c07bcf725c19920ef709df9d419f618152b03680
hulilei <huuuu1016@gmail.com>
2020-06-09 18:54:16 +0800

    版本号修改 日志添加

--- ft-sdk-ios/FTAutoTrack/FTAutoTrackVersion.h
+++ ft-sdk-ios/FTAutoTrack/FTAutoTrackVersion.h
@@ -8,7 +8,7 @@
 
 #ifndef FTAutoTrackVersion_h
 #define FTAutoTrackVersion_h
-#define SDK_VERSION  @"1.0.2-alpha.25"
+#define SDK_VERSION  @"1.0.2-alpha.26"
 
 
 #endif /* FTAutoTrackVersion_h */
--- ft-sdk-ios/FTMobileAgent/FTMobileAgentVersion.h
+++ ft-sdk-ios/FTMobileAgent/FTMobileAgentVersion.h
@@ -9,5 +9,5 @@
 #ifndef FTMobileAgentVersion_h
 #define FTMobileAgentVersion_h
 
-#define SDK_VERSION  @"1.0.2-alpha.25"
+#define SDK_VERSION  @"1.0.2-alpha.26"
 #endif /* FTMobileAgentVersion_h */
