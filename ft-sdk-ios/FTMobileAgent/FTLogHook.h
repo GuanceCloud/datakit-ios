b4f8676de2623644ac8573d0e974ed593b789a3e
hulilei <huuuu1016@gmail.com>
2020-06-15 19:53:21 +0800

    hook nslog frint方法添加

---
+++ ft-sdk-ios/FTMobileAgent/FTLogHook.h
@@ -0,0 +1,18 @@
+//
+//  FTLogHook.h
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2020/6/15.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTLogHook : NSObject
+
++ (void)hook;
+@end
+
+NS_ASSUME_NONNULL_END
