ee5b4a14cf24ee2aa729efaa912138fa7365a4b8
hulilei <huuuu1016@gmail.com>
2020-09-10 15:34:25 +0800

    删除重复测试代码

--- SampleApp/TestAutoTrack/DemoViewController.m
+++ SampleApp/TestAutoTrack/DemoViewController.m
@@ -11,12 +11,7 @@
 #import <FTMobileAgent/FTMobileAgent.h>
 #import "UITestManger.h"
 #import "AppDelegate.h"
-#import "TestFlowTrackVC.h"
-#import "TestSubFlowTrack.h"
-#import "TestSubFlowTrack2.h"
 #import "TestBluetoothList.h"
-#import "TestCustomTrackVC.h"
-#import "SampleApp-Swift.h"
 #import <FTMobileAgent/FTBaseInfoHander.h>
 #import <FTMobileAgent/NSDate+FTAdd.h>
 @interface DemoViewController ()<UITableViewDelegate,UITableViewDataSource>
@@ -31,7 +26,7 @@
     [super viewDidLoad];
     UIBarButtonItem *rightBarItem = [[UIBarButtonItem alloc] initWithTitle:@"确认" style:UIBarButtonItemStylePlain target:self action:@selector(onClickedOKbtn)];
     self.navigationItem.rightBarButtonItem = rightBarItem;
-    self.dataSource = @[@"BindUser",@"LogOut",@"Test_CustomTrack",@"Test_autoTrack",@"Test_resetConfig",@"Test_startLocation",@"Test_startMonitorFlush",@"Test_stopMonitorFlush",@"Test_getConnectBluetooth",@"Test_crashLog",@"Test_log",@"Test_NetworkTrace"];
+    self.dataSource = @[@"Test_autoTrack",@"Test_startMonitorFlush",@"Test_stopMonitorFlush",@"Test_getConnectBluetooth",@"Test_crashLog"];
     [self createUI];
 }
 - (void)onClickedOKbtn {
@@ -47,48 +42,9 @@
     
     [_mtableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
 }
-- (void)testStartLocation{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [FTMobileAgent startLocation:^(NSInteger errorCode, NSString * _Nullable errorMessage) {
-        
-            [self showResult:[NSString stringWithFormat:@"errorCode = %ld,errorMessage=%@",(long)errorCode,errorMessage]];
-        }];
-    });
-    
-}
-- (void)testBindUser{
-    [[FTMobileAgent sharedInstance] bindUserWithName:@"test8" Id:@"1111111" exts:@{@"platform": @"ios"}];
-}
-- (void)testUserLogout{
-    [[FTMobileAgent sharedInstance] logout];
-}
-- (void)testCustomTrack{
-    self.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:[TestCustomTrackVC new] animated:YES];
-    self.hidesBottomBarWhenPushed = NO;
-}
-
-- (void)testFlowTrack{
-    self.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:[TestFlowTrackVC new] animated:YES];
-    self.hidesBottomBarWhenPushed = NO;
-    
-}
 - (void)testAutoTrack{
-    
     self.hidesBottomBarWhenPushed = YES;
     [self.navigationController pushViewController:[UITestVC new] animated:YES];
-    self.hidesBottomBarWhenPushed = NO;
-}
-- (void)testSubFlowTrack{
-    self.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:[TestSubFlowTrack new] animated:YES];
-    self.hidesBottomBarWhenPushed = NO;
-    
-}
-- (void)testSubFlowTrack2{
-    self.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:[TestSubFlowTrack2 new] animated:YES];
     self.hidesBottomBarWhenPushed = NO;
 }
 -(void)showResult:(NSString *)title{
@@ -96,19 +52,6 @@
     UIAlertAction *commit = [UIAlertAction actionWithTitle:@"ok" style:UIAlertActionStyleCancel handler:nil];
     [alert addAction:commit];
     [self presentViewController:alert animated:YES completion:nil];
-}
-- (void)testResetConfig{
-    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
-    NSString *akId =[processInfo environment][@"ACCESS_KEY_ID"];
-    NSString *akSecret = [processInfo environment][@"ACCESS_KEY_SECRET"];
-    NSString *url = [processInfo environment][@"ACCESS_SERVER_URL"];
-    NSString *token = [processInfo environment][@"ACCESS_DATAWAY_TOKEN"];
-
-    // 新的config 关闭了autoTrack  将无全埋点日志
-    FTMobileConfig *config = [[FTMobileConfig alloc]initWithMetricsUrl:url datawayToken:token akId:akId akSecret:akSecret enableRequestSigning:YES];
-    config.enableLog = YES;
-    config.enableAutoTrack = NO;
-    [FTMobileAgent startWithConfigOptions:config];
 }
 - (void)testStartMonitorFlush{
     [[FTMobileAgent sharedInstance] startMonitorFlushWithInterval:10 monitorType:FTMonitorInfoTypeAll];
@@ -126,33 +69,6 @@
     NSString *value = nil;
     NSDictionary *dict = @{@"11":value};
 }
-- (void)testLog{
-    NSLog(@"testLog");
-    FrintHookTest *test = [[FrintHookTest alloc]init];
-    [test show];
-}
-- (void)testNetworkTrace{
-    NSArray *search = @[@"上海天气",@"鹅鹅鹅",@"温度",@"机器人"];
-    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
-    int x = arc4random() % 4;
-    NSString *parameters = [NSString stringWithFormat:@"key=free&appid=0&msg=%@",search[x]];
-    NSString *urlStr = @"http://api.qingyunke.com/api.php";
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlStr]];
-
-    request.HTTPMethod = @"POST";
-    [request addValue:[[NSDate date] ft_dateGMT] forHTTPHeaderField:@"Date"];
-    [request addValue:[[NSDate date] ft_dateGMT] forHTTPHeaderField:@"Date"];
-
-    request.HTTPBody = [parameters dataUsingEncoding:NSUTF8StringEncoding];
-    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSHTTPURLResponse *res =(NSHTTPURLResponse *)response;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self showResult:[NSString stringWithFormat:@"statusCode == %ld",(long)res.statusCode]];
-        });
-    }];
-    [task resume];
-}
 #pragma mark ========== UITableViewDataSource ==========
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.dataSource.count;
@@ -167,40 +83,19 @@
     NSUInteger row = [indexPath row];
     switch (row) {
         case 0:
-            [self testBindUser];
-            break;
-        case 1:
-            [self testUserLogout];
-            break;
-        case 2:
-            [self testCustomTrack];
-            break;
-        case 3:
             [self testAutoTrack];
             break;
-        case 4:
-            [self testResetConfig];
-            break;
-        case 5:
-            [self testStartLocation];
-            break;
-        case 6:
+        case 1:
             [self testStartMonitorFlush];
             break;
-        case 7:
+        case 2:
             [self testStopMonitorFlush];
             break;
-        case 8:
+        case 3:
             [self testConnectBluetooth];
             break;
-        case 9:
+        case 4:
             [self testCrashLog];
-            break;
-        case 10:
-            [self testLog];
-            break;
-        case 11:
-            [self testNetworkTrace];
             break;
         default:
             break;
