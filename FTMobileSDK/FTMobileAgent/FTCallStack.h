098effd75308bdaa782e49e78d31c8db94f0331f
hulilei <huuuu1016@gmail.com>
2020-10-09 18:52:20 +0800

    堆栈信息解析

---
+++ FTMobileSDK/FTMobileAgent/FTCallStack.h
@@ -0,0 +1,22 @@
+//
+//  FTANRMonitor.h
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2020/10/09.
+//  Copyright © 2020 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTCallStack : NSObject
+
++ (NSString *)ft_backtraceOfAllThread;
++ (NSString *)ft_backtraceOfCurrentThread;
++ (NSString *)ft_backtraceOfMainThread;
++ (NSString *)ft_backtraceOfNSThread:(NSThread *)thread;
+
+@end
+
+NS_ASSUME_NONNULL_END
