57d282323c8784cef0974000ce761ec566b04fab
hulilei <huuuu1016@gmail.com>
2020-12-15 10:46:22 +0800

    获取网络状态支持5G判断

--- FTMobileSDK/FTMobileAgent/Network/FTNetworkInfo.m
+++ FTMobileSDK/FTMobileAgent/Network/FTNetworkInfo.m
@@ -36,13 +36,13 @@ isPhoneX = [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bo
     if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
         CTTelephonyNetworkInfo *teleInfo= [[CTTelephonyNetworkInfo alloc] init];
         NSString *accessString = teleInfo.currentRadioAccessTechnology;
-//        if (@available(iOS 14.0, *)) {
-//            NSArray *typeStrings5G = @[CTRadioAccessTechnologyNRNSA,
-//                                       CTRadioAccessTechnologyNR];
-//            if ([typeStrings5G containsObject:accessString]) {
-//                return @"5G";
-//            }
-//        }
+        if (@available(iOS 14.0, *)) {
+            NSArray *typeStrings5G = @[CTRadioAccessTechnologyNRNSA,
+                                       CTRadioAccessTechnologyNR];
+            if ([typeStrings5G containsObject:accessString]) {
+                return @"5G";
+            }
+        }
         if ([typeStrings4G containsObject:accessString]) {
             return @"4G";
         } else if ([typeStrings3G containsObject:accessString]) {
