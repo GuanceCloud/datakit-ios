add9857fd1c13ddbb516626bff3962d1dcd32ab4
hulilei <huuuu1016@gmail.com>
2021-01-21 18:22:32 +0800

    FTWeakProxy弱代理 防止循环引用

---
+++ FTMobileSDK/FTMobileAgent/FTWeakProxy.h
@@ -0,0 +1,18 @@
+//
+//  FTWeakProxy.h
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2021/1/21.
+//  Copyright © 2021 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface FTWeakProxy : NSProxy
+@property (nullable, nonatomic, weak, readonly) id target;
+- (instancetype)initWithTarget:(id)target;
++ (instancetype)proxyWithTarget:(id)target;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FTMobileSDK/FTMobileAgent/FTWeakProxy.m
@@ -0,0 +1,76 @@
+//
+//  FTWeakProxy.m
+//  FTMobileAgent
+//
+//  Created by 胡蕾蕾 on 2021/1/21.
+//  Copyright © 2021 hll. All rights reserved.
+//
+
+#import "FTWeakProxy.h"
+
+@implementation FTWeakProxy
+- (instancetype)initWithTarget:(id)target {
+    _target = target;
+    return self;
+}
++ (instancetype)proxyWithTarget:(id)target {
+    return [[FTWeakProxy alloc] initWithTarget:target];
+}
+// 快速转发流程
+- (id)forwardingTargetForSelector:(SEL)selector {
+    return _target;
+}
+// 慢速转发流程
+- (void)forwardInvocation:(NSInvocation *)invocation {
+    void *null = NULL;
+    [invocation setReturnValue:&null];
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
+    return [NSObject instanceMethodSignatureForSelector:@selector(init)];
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    return [_target respondsToSelector:aSelector];
+}
+
+- (BOOL)isEqual:(id)object {
+    return [_target isEqual:object];
+}
+
+- (NSUInteger)hash {
+    return [_target hash];
+}
+
+- (Class)superclass {
+    return [_target superclass];
+}
+
+- (Class)class {
+    return [_target class];
+}
+
+- (BOOL)isKindOfClass:(Class)aClass {
+    return [_target isKindOfClass:aClass];
+}
+
+- (BOOL)isMemberOfClass:(Class)aClass {
+    return [_target isMemberOfClass:aClass];
+}
+
+- (BOOL)conformsToProtocol:(Protocol *)aProtocol {
+    return [_target conformsToProtocol:aProtocol];
+}
+
+- (BOOL)isProxy {
+    return YES;
+}
+
+- (NSString *)description {
+    return [_target description];
+}
+
+- (NSString *)debugDescription {
+    return [_target debugDescription];
+}
+@end
