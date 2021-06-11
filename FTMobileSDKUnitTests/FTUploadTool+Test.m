09a2061c32f77d4c8606d871426add0c7c7bcbf1
hulilei <hulilei@jiagouyun.com>
2021-06-11 14:13:02 +0800

    UnitTest添加FTUploadTool+Test分类禁止数据上传

---
+++ FTMobileSDKUnitTests/FTUploadTool+Test.m
@@ -0,0 +1,14 @@
+//
+//  FTUploadTool+Test.m
+//  FTMobileSDKUnitTests
+//
+//  Created by 胡蕾蕾 on 2021/6/11.
+//  Copyright © 2021 hll. All rights reserved.
+//
+
+#import "FTUploadTool+Test.h"
+
+@implementation FTUploadTool (Test)
+-(void)upload{
+}
+@end
