1127244ec93684b5c1e8f9270d665cf1d16df9e4
hulilei <hulilei@jiagouyun.com>
2021-06-21 18:59:01 +0800

    logger相关test添加

---
+++ FTMobileSDKUnitTests/AgentTest/FTLoggerTest.m
@@ -0,0 +1,189 @@
+//
+//  FTLoggerTest.m
+//  FTMobileSDKUnitTests
+//
+//  Created by 胡蕾蕾 on 2021/6/21.
+//  Copyright © 2021 hll. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <FTMobileAgent/FTMobileAgent.h>
+#import <FTDataBase/FTTrackerEventDBTool.h>
+#import <FTMobileAgent/FTMobileAgent+Private.h>
+#import <FTMobileAgent/NSDate+FTAdd.h>
+#import "FTUploadTool+Test.h"
+#import <FTMobileAgent/FTConstants.h>
+#import <FTJSONUtil.h>
+#import <FTRecordModel.h>
+#import "UITestVC.h"
+
+@interface FTLoggerTest : XCTestCase
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) UITestVC *testVC;
+@property (nonatomic, strong) UINavigationController *navigationController;
+@property (nonatomic, strong) UITabBarController *tabBarController;
+@property (nonatomic, copy) NSString *url;
+@property (nonatomic, copy) NSString *appid;
+@end
+
+@implementation FTLoggerTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.testVC = [[UITestVC alloc] init];
+    
+    self.tabBarController = [[UITabBarController alloc] init];
+    
+    self.navigationController = [[UINavigationController alloc] initWithRootViewController:self.testVC];
+    self.navigationController.tabBarItem.title = @"UITestVC";
+    
+    UITableViewController *firstViewController = [[UITableViewController alloc] init];
+    UINavigationController *firstNavigationController = [[UINavigationController alloc] initWithRootViewController:firstViewController];
+    
+    self.tabBarController.viewControllers = @[firstNavigationController, self.navigationController];
+    self.window.rootViewController = self.tabBarController;
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    self.url = [processInfo environment][@"ACCESS_SERVER_URL"];
+    self.appid = [processInfo environment][@"APP_ID"];
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [[FTMobileAgent sharedInstance] resetInstance];
+}
+- (void)testEnableCustomLog{
+    [self setRightSDKConfig];
+    NSInteger count =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.enableCustomLog = YES;
+    [[FTMobileAgent sharedInstance] startLoggerWithConfigOptions:loggerConfig];
+    [[FTMobileAgent sharedInstance] logging:@"testLoggingMethod" status:FTStatusInfo];
+    [NSThread sleepForTimeInterval:1];
+    NSInteger newCount =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    XCTAssertTrue(newCount>count);
+}
+- (void)testDisbleCustomLog{
+    [self setRightSDKConfig];
+    NSInteger count =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.enableCustomLog = NO
+    ;
+    [[FTMobileAgent sharedInstance] logging:@"testLoggingMethod" status:FTStatusInfo];
+    [NSThread sleepForTimeInterval:1];
+    NSInteger newCount =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    XCTAssertTrue(newCount == count);
+}
+- (void)testEnableTraceConsoleLog{
+    [self setRightSDKConfig];
+    NSInteger count =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.traceConsoleLog = YES;
+    NSLog(@"testEnableTraceConsoleLog");
+    [NSThread sleepForTimeInterval:1];
+    NSInteger newCount =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    XCTAssertTrue(newCount >= count);
+}
+- (void)testDisableTraceConsoleLog{
+    [self setRightSDKConfig];
+    NSInteger count =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.traceConsoleLog = NO;
+    NSLog(@"testDisableTraceConsoleLog");
+    [NSThread sleepForTimeInterval:1];
+    NSInteger newCount =  [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    XCTAssertTrue(newCount == count);
+}
+- (void)setRightSDKConfig{
+    FTMobileConfig *config = [[FTMobileConfig alloc]initWithMetricsUrl:self.url];
+    config.enableSDKDebugLog = YES;
+    [FTMobileAgent startWithConfigOptions:config];
+    [[FTMobileAgent sharedInstance] logout];
+    [[FTTrackerEventDBTool sharedManger] deleteItemWithTm:[[NSDate date] ft_dateTimestamp]];
+}
+-(void)testSetEmptyLoggerServiceName{
+    [self setRightSDKConfig];
+    FTMobileConfig *config = [[FTMobileConfig alloc]initWithMetricsUrl:self.url];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.enableCustomLog = YES;
+    [FTMobileAgent startWithConfigOptions:config];
+    [[FTMobileAgent sharedInstance] startLoggerWithConfigOptions:loggerConfig];
+    [[FTTrackerEventDBTool sharedManger] deleteItemWithTm:[[NSDate date] ft_dateTimestamp]];
+    [FTMobileAgent sharedInstance].upTool.isUploading = YES;
+    [[FTMobileAgent sharedInstance] logging:@"testSetEmptyServiceName" status:FTStatusInfo];
+    [NSThread sleepForTimeInterval:1];
+    NSArray *array = [[FTTrackerEventDBTool sharedManger] getFirstRecords:10 withType:FT_DATA_TYPE_LOGGING];
+    FTRecordModel *model = [array lastObject];
+    NSDictionary *dict = [FTJSONUtil dictionaryWithJsonString:model.data];
+    NSDictionary *op = dict[@"opdata"];
+    NSDictionary *tags = op[@"tags"];
+    NSString *serviceName = [tags valueForKey:FT_KEY_SERVICE];
+    XCTAssertTrue(serviceName.length>0);
+}
+
+-(void)testSetLoggerServiceName{
+    [self setRightSDKConfig];
+    FTMobileConfig *config = [[FTMobileConfig alloc]initWithMetricsUrl:self.url];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.service = @"testSetServiceName";
+    loggerConfig.enableCustomLog = YES;
+    [FTMobileAgent startWithConfigOptions:config];
+    [[FTMobileAgent sharedInstance] startLoggerWithConfigOptions:loggerConfig];
+    [FTMobileAgent sharedInstance].upTool.isUploading = YES;
+    [[FTTrackerEventDBTool sharedManger] deleteItemWithTm:[[NSDate date] ft_dateTimestamp]];
+    [[FTMobileAgent sharedInstance] logging:@"testSetEmptyServiceName" status:FTStatusInfo];
+    [NSThread sleepForTimeInterval:2];
+    NSArray *array = [[FTTrackerEventDBTool sharedManger] getFirstRecords:10 withType:FT_DATA_TYPE_LOGGING];
+    FTRecordModel *model = [array lastObject];
+    NSDictionary *dict = [FTJSONUtil dictionaryWithJsonString:model.data];
+    NSDictionary *op = dict[@"opdata"];
+    NSDictionary *tags = op[@"tags"];
+    NSString *serviceName = [tags valueForKey:FT_KEY_SERVICE];
+    XCTAssertTrue([serviceName isEqualToString:@"testSetServiceName"]);
+}
+- (void)testEnableLinkRumData{
+    [self setRightSDKConfig];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.enableLinkRumData = YES;
+    loggerConfig.enableCustomLog = YES;
+    FTRumConfig *rumConfig = [[FTRumConfig alloc]initWithAppid:self.appid];
+    [[FTMobileAgent sharedInstance] startLoggerWithConfigOptions:loggerConfig];
+    [[FTMobileAgent sharedInstance] startRumWithConfigOptions:rumConfig];
+    [self.testVC view];
+    [self.testVC viewDidAppear:NO];
+    [[FTMobileAgent sharedInstance] logging:@"testEnableLinkRumData" status:FTStatusInfo];
+
+    [NSThread sleepForTimeInterval:1];
+    NSArray *datas = [[FTTrackerEventDBTool sharedManger] getFirstRecords:10 withType:FT_DATA_TYPE_LOGGING];
+    FTRecordModel *model = [datas lastObject];
+    NSDictionary *dict = [FTJSONUtil dictionaryWithJsonString:model.data];
+    NSDictionary *opdata = dict[@"opdata"];
+    NSDictionary *tags =opdata[@"tags"];
+    XCTAssertTrue([tags.allKeys containsObject:@"session_id"]);
+    XCTAssertTrue([tags.allKeys containsObject:@"session_type"]);
+}
+- (void)testDisableLinkRumData{
+    [self setRightSDKConfig];
+    FTLoggerConfig *loggerConfig = [[FTLoggerConfig alloc]init];
+    loggerConfig.enableLinkRumData = NO;
+    loggerConfig.enableCustomLog = YES;
+    FTRumConfig *rumConfig = [[FTRumConfig alloc]initWithAppid:self.appid];
+    [[FTMobileAgent sharedInstance] startLoggerWithConfigOptions:loggerConfig];
+    [[FTMobileAgent sharedInstance] startRumWithConfigOptions:rumConfig];
+    [self.testVC view];
+    [self.testVC viewDidAppear:NO];
+    [[FTMobileAgent sharedInstance] logging:@"testEnableLinkRumData" status:FTStatusInfo];
+
+    [NSThread sleepForTimeInterval:1];
+    NSArray *datas = [[FTTrackerEventDBTool sharedManger] getFirstRecords:10 withType:FT_DATA_TYPE_LOGGING];
+    FTRecordModel *model = [datas lastObject];
+    NSDictionary *dict = [FTJSONUtil dictionaryWithJsonString:model.data];
+    NSDictionary *opdata = dict[@"opdata"];
+    NSDictionary *tags =opdata[@"tags"];
+    XCTAssertFalse([tags.allKeys containsObject:@"session_id"]);
+    XCTAssertFalse([tags.allKeys containsObject:@"session_type"]);
+
+}
+@end
