3745869480f9e64a60070068aeb45dd1b1ed184f
hulilei <huuuu1016@gmail.com>
2021-10-25 19:31:18 +0800

    pingthread 使用atomic管理属性线程安全

--- FTMobileSDK/FTMobileAgent/FTPingThread.m
+++ FTMobileSDK/FTMobileAgent/FTPingThread.m
@@ -10,11 +10,10 @@
 #import "FTCallStack.h"
 
 @interface FTPingThread ()
-@property (nonatomic, strong) NSLock *lock;
-@property (nonatomic, assign) BOOL isResponse;
+@property (atomic, assign) BOOL isResponse;
 @property (nonatomic, strong) dispatch_semaphore_t semaphore;
-@property (nonatomic, strong) NSDate *freezeStartDate;
-@property (nonatomic, copy) NSString *callSymbols;
+@property (atomic, strong) NSDate *freezeStartDate;
+@property (atomic, copy) NSString *callSymbols;
 @end
 @implementation FTPingThread
 
@@ -26,45 +25,6 @@
         _semaphore = dispatch_semaphore_create(0);
     }
     return _semaphore;
-}
--(NSLock *)lock{
-    if (!_lock) {
-        _lock = [[NSLock alloc]init];
-    }
-    return _lock;
-}
--(NSDate *)getFreezeStartDate{
-    [self.lock lock];
-    NSDate *freezeStartDate = _freezeStartDate;
-    [self.lock unlock];
-    return freezeStartDate;
-}
--(BOOL)getIsResponse{
-    [self.lock lock];
-    BOOL result = _isResponse;
-    [self.lock unlock];
-    return result;
-}
--(void)setIsResponse:(BOOL)isResponse{
-    [self.lock lock];
-    _isResponse = isResponse;
-    [self.lock unlock];
-}
--(void)setFreezeStartDate:(NSDate *)freezeStartDate{
-    [self.lock lock];
-    _freezeStartDate = freezeStartDate;
-    [self.lock unlock];
-}
--(void)setCallSymbols:(NSString *)callSymbols{
-    [self.lock lock];
-    _callSymbols = callSymbols;
-    [self.lock unlock];
-}
--(NSString *)getCallSymbols{
-    [self.lock lock];
-    NSString *callSymbols = _callSymbols;
-    [self.lock unlock];
-    return  callSymbols;
 }
 - (void)pingMainThread {
     while (!self.cancelled) {
