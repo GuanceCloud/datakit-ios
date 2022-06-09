77d60320a2d3184421168573139e41c7fcfcb487
hulilei <huuuu1016@gmail.com>
2022-06-09 18:38:43 +0800

    静态库公开的头文件添加到FTMobileAgent文件

--- FTMobileSDK/FTMobileAgent/FTMobileAgent.h
+++ FTMobileSDK/FTMobileAgent/FTMobileAgent.h
@@ -8,7 +8,10 @@
 
 #import <Foundation/Foundation.h>
 #import "FTMobileConfig.h"
-
+#import "FTExternalDataManager.h"
+#import "FTResourceMetricsModel.h"
+#import "FTResourceContentModel.h"
+#import "FTTraceManager.h"
 NS_ASSUME_NONNULL_BEGIN
 @interface FTMobileAgent : NSObject
 
