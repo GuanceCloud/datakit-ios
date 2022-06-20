e60b3c953df5950d733107ba3b3f5f7edb7d887b
hulilei <huuuu1016@gmail.com>
2022-06-20 13:53:02 +0800

    wkwebview hook 删除无用代码

--- FTMobileSDK/FTMobileAgent/AutoTrack/WKWebView+FTAutoTrack.m
+++ FTMobileSDK/FTMobileAgent/AutoTrack/WKWebView+FTAutoTrack.m
@@ -11,27 +11,13 @@
 #import "NSURLRequest+FTMonitor.h"
 #import "FTSwizzler.h"
 #import <objc/runtime.h>
-//static void dataflux_addInstanceMethod(SEL selector,SEL addSelector,Class fromClass, Class toClass) {
-//    NSCParameterAssert(fromClass);
-//    NSCParameterAssert(toClass);
-//    Method originalMethod = class_getInstanceMethod(toClass, selector);
-//    if(!originalMethod){
-//    Method method = class_getInstanceMethod(fromClass, addSelector);
-//    // 返回该方法的实现
-//    IMP methodIMP = method_getImplementation(method);
-//    // 获取该方法的返回类型
-//    const char *types = method_getTypeEncoding(method);
-//    // 在 toClass 中，添加方法
-//        class_addMethod(toClass, selector, methodIMP, types);
-//    }
-//}
 @implementation WKWebView (FTAutoTrack)
 
 -(WKNavigation *)dataflux_loadRequest:(NSURLRequest *)request{
     [[FTWKWebViewHandler sharedInstance] addScriptMessageHandlerWithWebView:self];
     if ([FTWKWebViewHandler sharedInstance].enableTrace) {
         NSURLRequest *newrequest = [request ft_NetworkTrace];
-        [[FTWKWebViewHandler sharedInstance] addWebView:self];
+        [[FTWKWebViewHandler sharedInstance] addWebView:self request:request];
         return  [self dataflux_loadRequest:newrequest];
     }else{
         return [self dataflux_loadRequest:request];
@@ -62,86 +48,11 @@
         return [self dataflux_reload];
     }
 }
-//-(void)dataflux_setNavigationDelegate:(id<WKNavigationDelegate>)navigationDelegate{
-//    if (navigationDelegate != nil) {
-//       Class realClass = [FTSwizzler realDelegateClassFromSelector:@selector(webView:decidePolicyForNavigationAction:decisionHandler:) proxy:navigationDelegate];
-//        SEL requestM = @selector(webView:decidePolicyForNavigationAction:decisionHandler:);
-//        SEL responseM = @selector(webView:decidePolicyForNavigationResponse:decisionHandler:);
-//        SEL loadErrorM = @selector(webView:didFailProvisionalNavigation:withError:);
-//
-//        if(!realClass.dataFlux_className){
-//            realClass.dataFlux_className = NSStringFromClass(realClass);
-//            if(![FTSwizzler realDelegateClass:realClass respondsToSelector:requestM]){
-//                dataflux_addInstanceMethod(requestM,@selector(dataflux_none_webView:decidePolicyForNavigationAction:decisionHandler:), WKWebView.class, realClass);
-//            }
-//            if(![FTSwizzler realDelegateClass:realClass respondsToSelector:responseM]){
-//                dataflux_addInstanceMethod(responseM,@selector(dataflux_none_webView:decidePolicyForNavigationResponse:decisionHandler:), WKWebView.class, realClass);
-//            }
-//            if(![FTSwizzler realDelegateClass:realClass respondsToSelector:loadErrorM]){
-//                dataflux_addInstanceMethod(loadErrorM,@selector(dataflux_none_webView:didFailProvisionalNavigation:withError:), WKWebView.class, realClass);
-//            }
-//
-//            [FTSwizzler swizzleSelector:requestM onClass:realClass withBlock:^(id instance, SEL method, WKWebView *webView, WKNavigationAction *navigationAction,id decisionHandler) {
-//                if ([FTWKWebViewHandler sharedInstance].enableTrace) {
-//                    [[FTWKWebViewHandler sharedInstance] addRequest:navigationAction.request webView:webView];
-//                }
-//
-//
-//            } named:@"dataflux_wkwebview_request"];
-//
-//            [FTSwizzler swizzleSelector:responseM onClass:realClass withBlock:^(id instance, SEL method, WKWebView *webView, WKNavigationResponse *navigationResponse,id decisionHandler) {
-//                if ([FTWKWebViewHandler sharedInstance].enableTrace) {
-//                    [[FTWKWebViewHandler sharedInstance] addResponse:navigationResponse.response webView:webView];
-//                }
-//
-//            } named:@"dataflux_wkwebview_response"];
-//            [FTSwizzler swizzleSelector:loadErrorM onClass:realClass withBlock:^(id instance, SEL method, WKWebView *webView, WKNavigation *navigation,NSError *error) {
-//                if ([FTWKWebViewHandler sharedInstance].enableTrace) {
-//                    [[FTWKWebViewHandler sharedInstance] didRequestFailWithError:error webView:webView];
-//                }
-//
-//            } named:@"dataflux_wkwebview_loadError"];
-//        }
-//       }
-//    [self dataflux_setNavigationDelegate:navigationDelegate];
-//}
+
 -(void)dataflux_dealloc{
     [[FTWKWebViewHandler sharedInstance] removeWebView:self];
     [self dataflux_dealloc];
 }
 
-//// request
-//-(void)dataflux_webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
-//    if ([FTWKWebViewHandler sharedInstance].enableTrace) {
-//        [[FTWKWebViewHandler sharedInstance] addRequest:navigationAction.request webView:webView];
-//    }
-//    //允许跳转
-//    decisionHandler(WKNavigationActionPolicyAllow);
-//}
-////response
-//-(void)dataflux_webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
-//    if ([FTWKWebViewHandler sharedInstance].enableTrace) {
-//        [[FTWKWebViewHandler sharedInstance] addResponse:navigationResponse.response webView:webView];
-//    }
-//    decisionHandler(WKNavigationResponsePolicyAllow);
-//}
-////load error
-//-(void)dataflux_webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error{
-//    if ([FTWKWebViewHandler sharedInstance].enableTrace) {
-//        [[FTWKWebViewHandler sharedInstance] didRequestFailWithError:error webView:webView];
-//    }
-//}
-// request
-//-(void)dataflux_none_webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{                                                                                                                                          decisionHandler(WKNavigationActionPolicyAllow);
-//}
-////response
-//-(void)dataflux_none_webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
-//
-//    decisionHandler(WKNavigationResponsePolicyAllow);
-//}
-////load error
-//-(void)dataflux_none_webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error{
-//
-//}
 @end
 
