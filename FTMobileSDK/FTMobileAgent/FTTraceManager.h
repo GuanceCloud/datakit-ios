6e015e2ccf371f4dc9594aea93e7ad4f17ee8362
hulilei <huuuu1016@gmail.com>
2022-11-08 19:00:48 +0800

    api 废弃提示修改

--- FTMobileSDK/FTMobileAgent/FTTraceManager.h
+++ FTMobileSDK/FTMobileAgent/FTTraceManager.h
@@ -13,7 +13,7 @@ DEPRECATED_ATTRIBUTE
 @interface FTTraceManager : NSObject
 + (instancetype)sharedInstance DEPRECATED_ATTRIBUTE;
 
-- (NSDictionary *)getTraceHeaderWithKey:(NSString *)key url:(NSURL *)url DEPRECATED_MSG_ATTRIBUTE("已过时，请参考 FTExternalDataManager 类的 -getTraceHeaderWithKey 方法");
+- (NSDictionary *)getTraceHeaderWithKey:(NSString *)key url:(NSURL *)url DEPRECATED_MSG_ATTRIBUTE("已过时，请使用 [[FTExternalDataManager sharedManager] getTraceHeaderWithKey:url:] 替换");
 @end
 
 NS_ASSUME_NONNULL_END
