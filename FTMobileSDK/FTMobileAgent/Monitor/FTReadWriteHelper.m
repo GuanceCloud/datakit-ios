1c4a732899fb434132c8571fb20d9f73eb272a19
hulilei <huuuu1016@gmail.com>
2023-01-29 18:46:42 +0800

    FTReadWriteHelper添加注释、读数据方法修改

--- FTMobileSDK/FTMobileAgent/Monitor/FTReadWriteHelper.m
+++ FTMobileSDK/FTMobileAgent/Monitor/FTReadWriteHelper.m
@@ -10,15 +10,12 @@
 @interface FTReadWriteHelper<ValueType>()
 @property (nonatomic, strong) ValueType value;
 @property (nonatomic, strong) dispatch_queue_t concurrentQueue;
-@property (nonatomic, strong) dispatch_semaphore_t semaphore;
 @end
 @implementation FTReadWriteHelper
 -(instancetype)initWithValue:(id)value{
     self = [super init];
     if (self) {
         _value = value;
-        _semaphore = dispatch_semaphore_create(0);
-
         _concurrentQueue = dispatch_queue_create([[NSString stringWithFormat:@"com.ft.value.readwrite.%@",_value] UTF8String], DISPATCH_QUEUE_CONCURRENT);
     }
     return self;
@@ -35,11 +32,9 @@
 }
 - (id)currentValue{
     __block id returnValue;
-    [self concurrentRead:^(id  _Nonnull value) {
-        returnValue = value;
-        dispatch_semaphore_signal(self.semaphore);
-    }];
-    dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER);
+    dispatch_sync(self.concurrentQueue, ^{
+        returnValue = self.value;
+    });
     return returnValue;
 }
 @end
