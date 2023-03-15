15c9e24abe5cff9bd1f57ea7552ff4bbde2cd480
hulilei <huuuu1016@gmail.com>
2023-03-15 18:11:33 +0800

    doccCoverage.sh 支持添加参数获取全部文件文档覆盖率

--- doccCoverage.sh
+++ doccCoverage.sh
@@ -1,6 +1,37 @@
+# sh doccCoverage.sh all  获取全部文档覆盖率
+# sh doccCoverage.sh      获取 public 文件 文档覆盖率
+project_path='FTMobileSDK.xcodeproj/project.pbxproj'
+copyFile="test.pbxproj"
 
-cd FTMobileSDK
-pwd
+changeFileAttributeToPublic(){
+states='init'
+lineNum=0
+cat -n $project_path | while read line
+do
+  lineNum=`expr $lineNum + 1`
+  if [[ $line =~ 'Begin PBXBuildFile section */' ]]
+  then
+  states='start'
+      echo $line
+  elif [[ $line =~ 'End PBXBuildFile section */' ]]
+  then
+  states='end'
+      echo $line
+  elif [[ $states = 'start' ]]
+  then
+    if [[ $line =~ '.h in Headers */' ]];then
+       if [[ $line =~ 'settings = {ATTRIBUTES = (Public,' ]];then
+           echo "已经是 Public 文件"
+       else
+           #brew install gnu-sed
+           sed -i "" "$lineNum s/\}\;/settings = \{ATTRIBUTES = \(Public, \)\; \}\; \}\;/" $project_path
+       fi
+    fi
+  fi
+done
+}
+
+doccCoverage(){
 xcodebuild -target FTMobileAgent DOCC_EXTRACT_SWIFT_INFO_FOR_OBJC_SYMBOLS=NO
 
 xcrun docc convert FTMobileSDKDocs.docc \
@@ -10,4 +41,26 @@ xcrun docc convert FTMobileSDKDocs.docc \
 --additional-symbol-graph-dir ./build/FTMobileSDK.build/Release-iphoneos/FTMobileAgent.build/symbol-graph \
 --experimental-documentation-coverage \
 --level detailed
+}
 
+# 若为 all 则获取所有文件的注释覆盖率
+FT_ALL_FILE_COVERAGE="$1"
+echo "----- Start -----"
+
+cd FTMobileSDK
+pwd
+
+if [[ "$FT_ALL_FILE_COVERAGE" == "all" ]]; then
+echo "-----changeFileAttributeToPublic Start-----"
+changeFileAttributeToPublic
+echo "-----changeFileAttributeToPublic End-----"
+fi
+
+echo "-----Coverage Start-----"
+doccCoverage
+echo "-----Coverage End-----"
+
+if [[ "$FT_ALL_FILE_COVERAGE" == "all" ]]; then
+git checkout -- $project_path
+echo "----- End -----"
+fi
