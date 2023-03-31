6036b4bd9f0757d6eee0d391e2fb101c9c7b2146
hulilei <huuuu1016@gmail.com>
2023-03-31 16:47:10 +0800

    脚本中路径修改

--- doccCoverage.sh
+++ doccCoverage.sh
@@ -34,7 +34,7 @@ done
 doccCoverage(){
 xcodebuild -target FTMobileAgent DOCC_EXTRACT_SWIFT_INFO_FOR_OBJC_SYMBOLS=NO
 
-xcrun docc convert FTMobileSDKDocs.docc \
+xcrun docc convert FTMobileSDK/FTMobileSDKDocs.docc \
 --fallback-display-name FTMobileAgent \
 --fallback-bundle-identifier com.cloudcare.ft.mobile.sdk.FTMobileAgent \
 --fallback-bundle-version 1.0 \
