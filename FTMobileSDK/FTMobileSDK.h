10b90431463c37d06ef7c92b93a4e1b03ed20e74
hulilei <huuuu1016@gmail.com>
2023-04-19 20:53:30 +0800

    解决 FTMobileSDK 打包无 module 问题

---
+++ FTMobileSDK/FTMobileSDK.h
@@ -0,0 +1,26 @@
+//
+//  FTMobileSDK.h
+//  FTMobileSDK
+//
+//  Created by hulilei on 2023/4/19.
+//  Copyright © 2023 DataFlux-cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FTMobileSDK.
+FOUNDATION_EXPORT double FTMobileSDKVersionNumber;
+
+//! Project version string for FTMobileSDK.
+FOUNDATION_EXPORT const unsigned char FTMobileSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTMobileSDK/PublicHeader.h>
+
+
+#import "FTMobileConfig.h"
+#import "FTExternalDataManager.h"
+#import "FTResourceMetricsModel.h"
+#import "FTResourceContentModel.h"
+#import "FTURLSessionDelegate.h"
+#import "FTTraceManager.h"
+#import "FTMobileAgent.h"
