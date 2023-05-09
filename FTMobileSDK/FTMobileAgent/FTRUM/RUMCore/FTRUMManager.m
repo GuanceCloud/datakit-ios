83afedc8a3de29d994e50fd40932178e431bf2cc
hulilei <huuuu1016@gmail.com>
2023-05-09 17:17:39 +0800

    resource_size 转换为 NSNumber 类型

--- FTMobileSDK/FTMobileAgent/FTRUM/RUMCore/FTRUMManager.m
+++ FTMobileSDK/FTMobileAgent/FTRUM/RUMCore/FTRUMManager.m
@@ -228,7 +228,9 @@
                     }else if([lowercasekey isEqualToString:@"content-encoding"]){
                         [tags setValue:content.responseHeader[key] forKey:FT_KEY_RESPONSE_CONTENT_ENCODING];
                     }else if ([lowercasekey isEqualToString:@"content-length"]){
-                        [fields setValue:content.responseHeader[key] forKey:FT_KEY_RESOURCE_SIZE];
+                        id size = content.responseHeader[lowercasekey];
+                        NSNumber *length = @([size integerValue]);
+                        [fields setValue:length forKey:FT_KEY_RESOURCE_SIZE];
                     }
                 }
             }
