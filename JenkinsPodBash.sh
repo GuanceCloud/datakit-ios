a0af8d64e7642683f447d37728d65332a7eb5aa2
章博 <brandonfaustine@gmail.com>
2023-05-19 10:30:45 +0800

    修改 github repo 地址

--- JenkinsPodBash.sh
+++ JenkinsPodBash.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(echo "$FT_PUSH_TAG" | sed -e 's/.*\///g' | sed -e 's/~.*//g' )
 
-git config remote.github.url >&- || git remote add github git@github.com:DataFlux-cn/datakit-ios.git
+git config remote.github.url >&- || git remote add github git@github.com:GuanceCloud/datakit-ios.git
 git push github $VERSION
 
 if [[ $? -eq 0 ]];then
