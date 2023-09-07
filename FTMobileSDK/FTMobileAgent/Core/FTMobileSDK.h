df2fb563ea52629ef2deeb7403392d9b972affac
hulilei <huuuu1016@gmail.com>
2023-09-07 20:14:29 +0800

    module 文件、public文件处理

---
+++ FTMobileSDK/FTMobileAgent/Core/FTMobileSDK.h
@@ -0,0 +1,19 @@
+//
+//  FTMobileSDK.h
+//  FTMobileSDK
+//
+//  Created by hulilei on 2023/4/19.
+//  Copyright © 2023 DataFlux-cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FTMobileConfig.h"
+#import "FTExternalDataManager.h"
+#import "FTResourceMetricsModel.h"
+#import "FTResourceContentModel.h"
+#import "FTURLSessionDelegate.h"
+#import "FTTraceManager.h"
+#import "FTLogger.h"
+#import "FTMobileAgent.h"
+#import "FTRumDatasProtocol.h"
---
+++ FTMobileSDK/include/FTRumDatasProtocol.h
@@ -0,0 +1,1 @@
+../FTSDKCore/Protocol/FTRumDatasProtocol.h
