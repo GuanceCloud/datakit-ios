bd9b13947d110816a02b15d050c8101ce329f833
hulilei <huuuu1016@gmail.com>
2023-11-15 14:12:47 +0800

    注释修改

--- FTMobileSDK/FTMobileAgent/Core/FTMobileAgent.h
+++ FTMobileSDK/FTMobileAgent/Core/FTMobileAgent.h
@@ -47,7 +47,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// 设置过滤 Resource 域名
 /// - Parameter handler: 判断是否采集回调，返回 YES 采集， NO 过滤掉
-- (void)isIntakeUrl:(BOOL(^)(NSURL *url))handler DEPRECATED_MSG_ATTRIBUTE("已废弃，请在配置 FTRumConfig 时设置 `isExcludeUrl` 进行替换");
+- (void)isIntakeUrl:(BOOL(^)(NSURL *url))handler DEPRECATED_MSG_ATTRIBUTE("已废弃，请在配置 FTRumConfig 时设置 `resourceUrlHandler` 进行替换");
 /// 配置 Trace Config 开启 Trace 功能
 ///
 /// - Parameters:
