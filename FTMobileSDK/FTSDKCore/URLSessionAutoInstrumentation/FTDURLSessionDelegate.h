35936d55e0caa943ebe29874143e1a4754449501
hulilei <huuuu1016@gmail.com>
2024-05-20 19:36:30 +0800

    hook到 URLSession 无 delegate，则设为 FTDURLSessionDelegate

---
+++ FTMobileSDK/FTSDKCore/URLSessionAutoInstrumentation/FTDURLSessionDelegate.h
@@ -0,0 +1,17 @@
+//
+//  FTDURLSessionDelegate.h
+//  FTMobileSDK
+//
+//  Created by hulilei on 2024/5/20.
+//  Copyright © 2024 DataFlux-cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTDURLSessionDelegate : NSObject<NSURLSessionTaskDelegate,NSURLSessionDataDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FTMobileSDK/FTSDKCore/URLSessionAutoInstrumentation/FTDURLSessionDelegate.m
@@ -0,0 +1,27 @@
+//
+//  FTDURLSessionDelegate.m
+//  FTMobileSDK
+//
+//  Created by hulilei on 2024/5/20.
+//  Copyright © 2024 DataFlux-cn. All rights reserved.
+//
+#import "FTURLSessionInstrumentation.h"
+#import "FTDURLSessionDelegate.h"
+
+@implementation FTDURLSessionDelegate
+-(void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data{
+    if(FTURLSessionInstrumentation.sharedInstance.shouldRUMInterceptor){
+        [FTURLSessionInstrumentation.sharedInstance.interceptor taskReceivedData:dataTask data:data];
+    }
+}
+-(void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didFinishCollectingMetrics:(NSURLSessionTaskMetrics *)metrics{
+    if(FTURLSessionInstrumentation.sharedInstance.shouldRUMInterceptor){
+        [FTURLSessionInstrumentation.sharedInstance.interceptor taskMetricsCollected:task metrics:metrics];
+    }
+}
+-(void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error{
+    if(FTURLSessionInstrumentation.sharedInstance.shouldRUMInterceptor){
+        [FTURLSessionInstrumentation.sharedInstance.interceptor taskCompleted:task error:error];
+    }
+}
+@end
