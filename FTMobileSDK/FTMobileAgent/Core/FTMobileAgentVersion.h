2b89d83df44f388bbee3a8819555d66cf93bbcf5
hulilei <huuuu1016@gmail.com>
2024-05-28 20:51:46 +0800

    版本号修改、日志添加

--- FTMobileSDK/FTMobileAgent/Core/FTMobileAgentVersion.h
+++ FTMobileSDK/FTMobileAgent/Core/FTMobileAgentVersion.h
@@ -9,6 +9,6 @@
 #ifndef FTMobileAgentVersion_h
 #define FTMobileAgentVersion_h
 
-#define SDK_VERSION  @"1.4.14-beta.1"
+#define SDK_VERSION  @"1.4.15-alpha.1"
 
 #endif /* FTMobileAgentVersion_h */
