23b9312ce3977832dabfd11b8946536bec6ebbbf
hulilei <huuuu1016@gmail.com>
2024-07-25 18:51:35 +0800

    example 添加采集UI控件示例

--- Examples/App/UITestVC.m
+++ Examples/App/UITestVC.m
@@ -9,7 +9,10 @@
 #import "UITestVC.h"
 #import "AppDelegate.h"
 @interface UITestVC ()<UICollectionViewDelegate,UICollectionViewDataSource>
-
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UISlider *slider;
+@property (nonatomic, strong) UIProgressView *progressView;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
 @end
 
 @implementation UITestVC
@@ -30,57 +33,74 @@
     _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)/2)];
     _scrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.view addSubview:_scrollView];
-    
-    _firstButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    _firstButton.frame = CGRectMake(x, 26, 100, 44);
-    [_firstButton setTitle:@"FirstButton" forState:UIControlStateNormal];
-    [_firstButton setTitle:@"SelectedFirstButton" forState:UIControlStateSelected];
+    _segmentedControl.frame = CGRectMake(100, 0, 100, 40);
+    _segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"first", @"second", @"third"]];
+    [_segmentedControl addTarget:self action:@selector(segmentedAction:) forControlEvents:UIControlEventValueChanged];
+    [_scrollView addSubview:_segmentedControl];
+    _firstButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _firstButton.frame = CGRectMake(x, 40, 100, 30);
+    [_firstButton setTitle:@"ActivityStart" forState:UIControlStateNormal];
+    [_firstButton setTitleColor:[UIColor systemBlueColor] forState:UIControlStateNormal];
     [_firstButton addTarget:self action:@selector(firstAction:) forControlEvents:UIControlEventTouchUpInside];
     _firstButton.isAccessibilityElement = YES;
+    _firstButton.layer.borderWidth = 1;
+    _firstButton.layer.borderColor = [UIColor grayColor].CGColor;
     [_scrollView addSubview:_firstButton];
-    UIButton *result = [UIButton buttonWithType:UIButtonTypeCustom];
-    result.frame = CGRectMake(width-100, 26, 100, 44);
-    [result setTitle:@"result" forState:UIControlStateNormal];
-    [result setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [result addTarget:self action:@selector(resultAction:) forControlEvents:UIControlEventTouchUpInside];
-//    result.accessibilityLabel = @"NEXT_CLICK";
-//    result.isAccessibilityElement = YES;
-    [self.scrollView addSubview:result];
+  
     
-    y = CGRectGetMaxY(_firstButton.frame) + 16;
+    y = CGRectGetMaxY(_firstButton.frame) + 22;
     _secondButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    _secondButton.frame = CGRectMake(x, y, 100, 44);
-    [_secondButton setTitle:@"SecondButton" forState:UIControlStateNormal];
-    [_secondButton setTitle:@"SelectedSecondButton" forState:UIControlStateSelected];
+    _secondButton.frame = CGRectMake(x, y, 100, 30);
+    [_secondButton setTitle:@"ActivityEnd" forState:UIControlStateNormal];
     [_secondButton addTarget:self action:@selector(secondAction:) forControlEvents:UIControlEventTouchUpInside];
     _secondButton.isAccessibilityElement = YES;
     [_scrollView addSubview:_secondButton];
-    UITextField *text = [[UITextField alloc]initWithFrame:CGRectMake(x+200, y, 100, 44)];
+    UITextField *text = [[UITextField alloc]initWithFrame:CGRectMake(x+200, 40, 100, 30)];
     text.backgroundColor = [UIColor grayColor];
     [_scrollView addSubview:text];
+    _textField = text;
     y = CGRectGetMaxY(_secondButton.frame) + 16;
     _stepper = [[UIStepper alloc] initWithFrame:CGRectMake(x, y, 80, 40)];
     [_stepper addTarget:self action:@selector(stepperAction:) forControlEvents:UIControlEventValueChanged];
     [_scrollView addSubview:_stepper];
     
-    
-    
     _uiswitch = [[UISwitch alloc] init];
-    _uiswitch.frame = CGRectMake(CGRectGetMaxX(_stepper.frame)+50, y, 80, 40);
+    _uiswitch.frame = CGRectMake(CGRectGetMaxX(_stepper.frame)+30, y, 80, 40);
     [_uiswitch addTarget:self action:@selector(switchAction:) forControlEvents:UIControlEventValueChanged];
     [_scrollView addSubview:_uiswitch];
     
-    _segmentedControl.frame = CGRectMake(100, 100, 100, 40);
-    _segmentedControl.backgroundColor = [UIColor blueColor];
-    _segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"first", @"second", @"third"]];
-    [_segmentedControl addTarget:self action:@selector(segmentedAction:) forControlEvents:UIControlEventValueChanged];
-    [_scrollView addSubview:_segmentedControl];
+    _slider = [[UISlider alloc]init];
+    _slider.frame = CGRectMake(CGRectGetMaxX(_uiswitch.frame)+30, y, 120, 40);
+    _slider.minimumValue = 0;
+    _slider.maximumValue = 10;
+    _slider.value = 5;
+    [_scrollView addSubview:_slider];
     
+    _progressView = [[UIProgressView alloc]init];
+    _progressView.frame = CGRectMake(CGRectGetMaxX(_uiswitch.frame)+30, y-40, 120, 20);
+    _progressView.progress = 0.5;
+    [_scrollView addSubview:_progressView];
+    CGRect frame = CGRectMake(160, 0, 200, 40);
+    // 创建 UIDatePicker 对象
+    UIDatePicker *datePicker = [[UIDatePicker alloc] initWithFrame:frame];
+    // 设置日期选择器模式:日期模式
+    datePicker.datePickerMode = UIDatePickerModeDate;
+    // 设置可供选择的最小时间：昨天
+    NSTimeInterval time = 24 * 60 * 60; // 24H 的时间戳值
+    datePicker.minimumDate = [[NSDate alloc] initWithTimeIntervalSinceNow:- time];
+    // 设置可供选择的最大时间：明天
+    datePicker.maximumDate = [[NSDate alloc] initWithTimeIntervalSinceNow:time];
+    // 添加 Target-Action
+    [datePicker addTarget:self
+                   action:@selector(datePickerValueChanged:)
+         forControlEvents:UIControlEventValueChanged];
+    // 将 UIDatePicker 对象添加到当前视图
+    [_scrollView addSubview:datePicker];
     y = CGRectGetMaxY(_uiswitch.frame) + 20;
     _label = [[UILabel alloc] initWithFrame:CGRectMake(x, y, width, 50)];
     _label.textAlignment = NSTextAlignmentCenter;
     _label.backgroundColor = [UIColor orangeColor];
-    _label.text = @"lable";
+    _label.text = @"label";
     _label.userInteractionEnabled = YES;
     _label.accessibilityLabel = @"LABLE_CLICK";
     _label.isAccessibilityElement = YES;
@@ -117,36 +137,47 @@
 
     [_collectionView registerClass:UICollectionViewCell.class forCellWithReuseIdentifier:@"UICollectionViewCell"];
     [self.view addSubview:_collectionView];
+}
+- (void)datePickerValueChanged:(id)sender{
     
 }
-
 - (void)firstAction:(UIButton *)sender {
-//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"测试" message:@"测试alert按钮点击" preferredStyle:UIAlertControllerStyleAlert];
-//    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-//        NSLog(@"点击OK");
-//    }];
-//    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"CANCEL" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-//        NSLog(@"点击CANCEL");
-//    }];
-//    [alert addAction:action];
-//    [alert addAction:cancel];
-//    [self presentViewController:alert animated:YES completion:nil];
     NSLog(@"%@ Touch Up Inside", sender.currentTitle);
-
+    if (@available(iOS 13.0, *)) {
+        if(!_activityIndicator){
+            UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
+            activityIndicator.frame = CGRectMake(0, 0, 50, 50);
+            
+            // 设置颜色
+            activityIndicator.color = [UIColor grayColor];
+            
+            // 设置位置
+            activityIndicator.center = self.view.center;
+            
+            // 添加到视图
+            [self.view addSubview:activityIndicator];
+            _activityIndicator = activityIndicator;
+        }
+        [_activityIndicator startAnimating];
+    }
 }
 
 - (void)secondAction:(UIButton *)sender {
     NSLog(@"%@ Touch Up Inside", sender.currentTitle);
+    [self.activityIndicator stopAnimating];
 }
 - (void)resultAction:(UIButton *)sender{
     
 }
 - (void)stepperAction:(UIStepper *)sender {
     NSLog(@"UIStepper on:%f", sender.value);
+    _slider.value = sender.value;
+    [_progressView setProgress:_slider.value/10 animated:YES];
 }
 
 - (void)switchAction:(UISwitch *)sender {
     NSLog(@"UISwitch on:%d", sender.isOn);
+    [_textField resignFirstResponder];
 }
 
 - (void)segmentedAction:(UISegmentedControl *)sender {
