6e6ea9f3c42ef9af5ed2708f8b609b50c3d2447e
hulilei <huuuu1016@gmail.com>
2024-10-18 18:58:42 +0800

    测试用例调整

--- Examples/FTMobileSDKUnitTests/OpenAPITest/FTRUMTest.m
+++ Examples/FTMobileSDKUnitTests/OpenAPITest/FTRUMTest.m
@@ -706,14 +706,14 @@
     
     [[FTGlobalRumManager sharedInstance].rumManager syncProcess];
     NSArray *newArray = [[FTTrackerEventDBTool sharedManger] getFirstRecords:10 withType:FT_DATA_TYPE_RUM];
-    __block BOOL hasDatas = NO;
+    __block int hasDatasCount = 0;
     [FTModelHelper resolveModelArray:newArray callBack:^(NSString * _Nonnull source, NSDictionary * _Nonnull tags, NSDictionary * _Nonnull fields, BOOL * _Nonnull stop) {
         if ([source isEqualToString:FT_RUM_SOURCE_ERROR]) {
-            hasDatas = YES;
+            hasDatasCount ++;
             *stop = YES;
         }
     }];
-    XCTAssertFalse(hasDatas);
+    XCTAssertTrue(hasDatasCount == 1);
 }
 #pragma mark ========== Long Task ==========
 
